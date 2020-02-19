0570e0ba432070eacd39a909ada33df20035f999
Pablo Hoc <hoc.pablo@gmail.com>
2020-02-19 16:38:40 -0300

    Update lista-de-tareas.js

--- soluciones/bucles/lista-de-tareas.js
+++ soluciones/bucles/lista-de-tareas.js
@@ -3,20 +3,22 @@ let programaFinalizado = false;
 
 while(!programaFinalizado) {
   let opcion = prompt(`Ingrese una opción:
-    - AGREGAR TAREA
-    - MODIFICAR TAREA
-    - ELIMINAR TAREA
-    - VER TAREAS
-    - SALIR
+   - [A]GREGAR TAREA
+   - [M]ODIFICAR TAREA
+   - [E]LIMINAR TAREA
+   - [V]ER TAREAS
+   - [S]ALIR
   `);
   
   switch(opcion) {
+    case 'A':
     case 'AGREGAR TAREA':
       let tarea = prompt('Ingrese una tarea');
       tareas.push(tarea);
       break;
+    case 'M':  
     case 'MODIFICAR TAREA':
-      let tareaAModificar = Number(prompt('Ingrese número de tarea a modficar');
+      let tareaAModificar = Number(prompt('Ingrese número de tarea a modficar'));
       if (tareas[tareaAModificar]) {
         let modificacion = prompt('La tarea actual es: "${tareas[tareaAModificar]}". Ingrese la modificación:');
         tareas[tareaAModificar] = modificacion;
@@ -24,10 +26,11 @@ while(!programaFinalizado) {
         alert('Tarea no encontrada');
       }
       break;
+    case 'E':  
     case 'ELIMINAR TAREA':
-      let tareaAEliminar = Number(prompt('Ingrese número de tarea a eliminar');
+      let tareaAEliminar = Number(prompt('Ingrese número de tarea a eliminar'));
       if (tareas[tareaAEliminar]) {
-        const elimina = confirm('La tarea a eliminar es: "${tareas[tareaAModificar]}". Realmente desea eliminarla?');
+        const elimina = confirm('La tarea a eliminar es: "${tareas[tareaAEliminar]}". Realmente desea eliminarla?');
         if (elimina) {
           elimina.splice(tareaAEliminar, 1);
         }
@@ -35,13 +38,15 @@ while(!programaFinalizado) {
         alert('Tarea no encontrada');
       }
       break;
+    case 'V':  
     case 'VER TAREAS':
       let listaDeTareas = 'TAREAS:\n';
       for (let i = 0; i < tareas.length; i++) {
-        listaDeTareas = `[${i}] "tareas[i]" \n`;
+        listaDeTareas += `[${i}] "${tareas[i]}" \n`;
       }
       alert(listaDeTareas);
       break;
+    case 'S':  
     case 'SALIR':
       const finaliza = confirm('Realmente desea finalizar el programa?');
       if (finaliza) {
