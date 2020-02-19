4e1954fff45ebe7b394b6af605e48e5a6df31e88
Pablo Hoc <hoc.pablo@gmail.com>
2020-02-19 16:14:38 -0300

    Create simon-dice-II.js

---
+++ soluciones/bucles/simon-dice-II.js
@@ -0,0 +1,25 @@
+const colores = ['rojo', 'azul', 'amarillo', 'verde'];
+const secuencia = [];
+
+let juegoTerminado = false;
+
+while(!juegoTerminado) {
+  // Obtengo un color aleatorio
+  const random = Math.round(Math.random() * (colores.length - 1));
+  const color = colores[random];
+  
+  // Lo agrego a la secuencia
+  secuencia.push(color);
+  
+  // Muestro la ronda y la secuencia actual (la ronda es la cantidad de colores en la secuencia)
+  alert(`Ronda ${secuencia.length}. La secuencia actual es: ${secuencia}`);
+  
+  for(let i = 0; i < secuencia.length; i++) {
+      const jugada = prompt(`Ingrese un color (${colores}):`);
+      if (jugada !== secuencia[i]) {
+        juegoTerminado = true;
+      }
+  }
+}
+
+alert(`¡Le has errado! Has sobrevivido ${secuencia.length} rondas`);
