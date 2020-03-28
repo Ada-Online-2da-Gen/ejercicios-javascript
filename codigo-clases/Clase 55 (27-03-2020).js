d69625fb88c581b0fe6963be3941e7791bff33c2
Pablo Hoc <hoc.pablo@gmail.com>
2020-03-28 03:12:19 -0300

    Update Clase 55 (27-03-2020).js

--- codigo-clases/Clase 55 (27-03-2020).js
+++ codigo-clases/Clase 55 (27-03-2020).js
@@ -15,3 +15,47 @@ const palabraConMasPuntaje = (palabras) => {
 
   return palabras.reduce(aPalabraConMasPuntaje)
 }
+
+// -------------------------------------------------
+
+const animales = [
+  { nombre: 'Sapo' }, 
+  { nombre: 'Leon' },
+  { nombre: 'Foca' },
+  { nombre: 'Sapo' }
+]
+
+// const cantidades = [
+//   { nombre: 'Sapo', repeticiones: 2 }, 
+//   { nombre: 'Leon', repeticiones: 1 }, 
+//   { nombre: 'Foca', repeticiones: 1 } 
+// ]
+
+const aRepeticiones = (cuentaParcial, animal) => {
+  const existe = cuentaParcial.some(cantidad => cantidad.nombre === animal.nombre)
+
+  if (!existe) {
+    return [...cuentaParcial, {nombre: animal.nombre, repeticiones: 1}]
+  } else {
+    return cuentaParcial.map(
+      cuenta => 
+        cuenta.nombre === animal.nombre ?
+        {
+          ...cuenta, 
+          repeticiones: cuenta.repeticiones + 1
+        } :
+        cuenta
+    )
+  }
+}
+
+const cantidades = animales.reduce(aRepeticiones, [])
+
+const aMayorRepeticion = (mayor, cantidad) => 
+  cantidad.repeticiones > mayor ? cantidad.repeticiones : mayor
+
+const conMasRepeticiones = (item) => item.repeticiones === masRepeticiones
+
+const masRepeticiones = cantidades.reduce(aMayorRepeticion, 0)
+
+const animalesFiltrados = cantidades.filter(conMasRepeticiones)
