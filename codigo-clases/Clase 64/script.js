5abb8a98cbee4ac70ea7f31d5fe92516e63ae490
Pablo Hoc <hoc.pablo@gmail.com>
2020-04-21 02:22:37 -0300

    Actualizo script clase 64

--- codigo-clases/Clase 64/script.js
+++ codigo-clases/Clase 64/script.js
@@ -19,18 +19,61 @@ const actualizarGatito = (data) => {
   img.height = data.height
 }
 
+const actualizarRazas = (razas) => {
+  const dropdown = document.querySelector('.dropdown-content')
+  dropdown.innerHTML = ''
+
+  const htmlRazas = razas
+    .map((raza) => `<a class="dropdown-item">${raza.name}</a>`)
+    .join('')
+
+  dropdown.innerHTML = htmlRazas
+}
+
 const obtenerGatitoRandom = () => {
   axios
     .get('https://api.thecatapi.com/v1/images/search')
     .then((response) => actualizarGatito(response.data[0]))
 }
 
+const obtenerRazas = () => {
+  axios
+    .get('https://api.thecatapi.com/v1/breeds')
+    .then((response) => actualizarRazas(response.data))
+}
+
+// query params
+
+// * www.mipagina.com/?param=valor&otro=valor
+
+const actualizarRaza = (raza) => {
+  document.getElementById('breed-description').innerHTML = raza.description
+}
+
+const obtenerRaza = (raza) => {
+  axios
+    .get(`https://api.thecatapi.com/v1/breeds/search?q=${raza}`)
+    .then((response) => actualizarRaza(response.data[0]))
+}
+
 window.onload = () => {
   // Promesa o promise
-  const botonGatoAleatorio = document.getElementById('random-cat-btn')
-  botonGatoAleatorio.addEventListener('click', obtenerGatitoRandom)
+  // const botonGatoAleatorio = document.getElementById('random-cat-btn')
+  // botonGatoAleatorio.addEventListener('click', obtenerGatitoRandom)
 
-  obtenerGatitoRandom()
+  document.querySelector('.dropdown').addEventListener('click', (event) => {
+    document.querySelector('.dropdown').classList.toggle('is-active')
+  })
+
+  document.addEventListener('click', (event) => {
+    if (event.target.className === 'dropdown-item') {
+      obtenerRaza(event.target.innerText)
+    }
+  })
+
+  obtenerRazas()
+
+  // obtenerGatitoRandom()
 }
 
 // // asincrona
